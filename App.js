c75106c9fbdce439171061b2d53f9e24d33da94a
mario-galindo <mgalindo9410@gmail.com>
2020-12-12 22:20:42 -0600

    components folder added

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Instagram Kuenta!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <NavigationContainer></NavigationContainer>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
