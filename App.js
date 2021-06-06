34136e540c9f74a3a2e8a588ed426c6efd666f5e
mario-galindo <mgalindo9410@gmail.com>
2021-06-05 22:42:24 -0600

    firebase config added and react-native-screens package added

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+import * as firebase from "firebase";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LandingScreen from "./components/auth/Landing";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSZserBUBfBduExqrsrGM4MIcMJi5bjKE",
+  authDomain: "instagram-kuenta.firebaseapp.com",
+  projectId: "instagram-kuenta",
+  storageBucket: "instagram-kuenta.appspot.com",
+  messagingSenderId: "419741061222",
+  appId: "1:419741061222:web:ca45629feaf499fe325da7",
+  measurementId: "G-SH3GW4GXTJ",
+};
 
 const Stack = createStackNavigator();
 
