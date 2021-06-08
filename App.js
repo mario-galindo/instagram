0eb3bb0d666104bb88d1e316d4e43e9a633daa1e
mario-galindo <mgalindo9410@gmail.com>
2021-06-08 01:05:30 -0600

    passing image prop to save screen

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import LandingScreen from "./components/auth/Landing";
 import RegisterScreen from "./components/auth/Register";
 import MainScreen from "./components/Main";
 import AddScreen from "./components/main/Add";
+import SaveScreen from './components/main/Save';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDSZserBUBfBduExqrsrGM4MIcMJi5bjKE",
@@ -96,7 +97,8 @@ export class App extends Component {
               component={MainScreen}
               options={{ headerShown: true }}
             />
-            <Stack.Screen name="Add" component={AddScreen} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation} />
+            <Stack.Screen name="Save" component={SaveScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
---
+++ components/main/Save.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View} from 'react-native'
+
+export default function Save(props) {
+
+    console.log(props.route.params.image)
+
+    return (
+        <View>
+            
+        </View>
+    )
+}
